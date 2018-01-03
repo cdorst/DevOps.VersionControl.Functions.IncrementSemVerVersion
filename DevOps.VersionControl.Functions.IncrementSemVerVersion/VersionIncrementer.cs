c11bc0a7cbbfb7a92841594d512cbcc99b1922e5
Christopher Dorst <chris@dorst.email>
2018-01-03 06:43:03 -0600

    Add VersionIncrementer

---
+++ DevOps.VersionControl.Functions.IncrementSemVerVersion/VersionIncrementer.cs
@@ -0,0 +1,24 @@
+﻿using DevOps.VersionControl.Structures.SemVer;
+
+namespace DevOps.VersionControl.Functions.IncrementSemVerVersion
+{
+    public static class VersionIncrementer
+    {
+        public static SemVerVersion Increment(SemVerVersion version, SemVerComponent component = SemVerComponent.Patch)
+        {
+            switch (component)
+            {
+                case SemVerComponent.Major:
+                    version.Major = version.Major + 1;
+                    break;
+                case SemVerComponent.Minor:
+                    version.Minor = version.Minor + 1;
+                    break;
+                case SemVerComponent.Patch:
+                    version.Patch = version.Patch + 1;
+                    break;
+            }
+            return version;
+        }
+    }
+}
